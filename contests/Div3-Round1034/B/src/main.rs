50eb33706b319dc3e4e5812bd98624ffd20c2ca3
lyteabovenyte <lyteabovenyte@gmail.com>
2025-07-24 10:38:11 +0330

    div3 1034 B

--- contests/Div3-Round1034/B/src/main.rs
+++ contests/Div3-Round1034/B/src/main.rs
@@ -1,3 +1,41 @@
+use std::io::{self, BufRead};
+
 fn main() {
-    println!("Hello, world!");
+    let stdin = io::stdin();
+    let mut lines = stdin.lock().lines();
+
+    let t: usize = lines.next().unwrap().unwrap().trim().parse().unwrap();
+
+    for _ in 0..t {
+        // Read n, j, k
+        let header = lines.next().unwrap().unwrap();
+        let mut parts = header.split_whitespace();
+        let n: usize = parts.next().unwrap().parse().unwrap();
+        let j: usize = parts.next().unwrap().parse().unwrap();
+        let k: usize = parts.next().unwrap().parse().unwrap();
+
+        // Read strengths
+        let mut strengths: Vec<usize> = lines
+            .next()
+            .unwrap()
+            .unwrap()
+            .split_whitespace()
+            .map(|x| x.parse().unwrap())
+            .collect();
+
+        let target = strengths[j - 1];
+
+        strengths.sort_unstable();
+        strengths.reverse();
+        // Count number of players strictly stronger
+        if k > 1 {
+            println!("Yes");
+        } else {
+            if strengths[0] == target {
+                println!("Yes");
+            } else {
+                println!("No");
+            }
+        }
+    }
 }
