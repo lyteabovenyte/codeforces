b2bc7323d5c23c63bfa60eaebefa5ff909fd4ba9
lyteabovenyte <lyteabovenyte@gmail.com>
2025-07-25 00:41:47 +0330

    1034 Div3 C

--- contests/Div3-Round1034/C/src/main.rs
+++ contests/Div3-Round1034/C/src/main.rs
@@ -1,3 +1,40 @@
+use std::io::{self, BufRead};
+
 fn main() {
-    println!("Hello, world!");
+    let stdin = io::stdin();
+    let mut input = stdin.lock().lines().map(|l| l.unwrap());
+
+    let t: usize = input.next().unwrap().parse().unwrap();
+
+    for _ in 0..t {
+        let n: usize = input.next().unwrap().parse().unwrap();
+        let a: Vec<i32> = input
+            .next()
+            .unwrap()
+            .split_whitespace()
+            .map(|x| x.parse().unwrap())
+            .collect();
+
+        let mut out = vec!['0'; n];
+
+        // From left to right: maintain min
+        let mut min_val = a[0];
+        for i in 0..n {
+            min_val = min_val.min(a[i]);
+            if min_val >= a[i] {
+                out[i] = '1';
+            }
+        }
+
+        // From right to left: maintain max
+        let mut max_val = a[n - 1];
+        for i in (0..n).rev() {
+            max_val = max_val.max(a[i]);
+            if max_val <= a[i] {
+                out[i] = '1';
+            }
+        }
+
+        println!("{}", out.iter().collect::<String>());
+    }
 }
