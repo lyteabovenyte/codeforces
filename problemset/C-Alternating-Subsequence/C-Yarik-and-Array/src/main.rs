5d6a0135a677eb5800f06bdf32d12ba3f02176a1
lyteabovenyte <lyteabovenyte@gmail.com>
2025-09-05 02:54:42 +0330

    ...

---
+++ problemset/C-Alternating-Subsequence/C-Yarik-and-Array/src/main.rs
@@ -0,0 +1,28 @@
+/*
+    Author: lyteabovenyte
+*/
+
+use std::io::{self, BufRead};
+
+fn main() {
+    let stdin = io::stdin();
+    let mut lines = stdin.lock().lines();
+
+    let t: i64 = lines.next().unwrap().unwrap().trim().parse().unwrap();
+    for _ in 0..t {
+        let n: i64 = lines.next().unwrap().unwrap().trim().parse().unwrap();
+        let a: Vec<i64> = lines.next().unwrap().unwrap().trim().split_whitespace().map(|s| s.parse().unwrap()).collect();
+    }
+}
+
+fn solve(a: Vec<i64>, n: i64) -> i64 {
+    let mut total_mx = 0
+    
+    let mut i = 0'
+    while i < n as usize {
+        let mut cur = a[i];
+        let mut j = i;
+
+        while j < n as usize && (a[j] % 2) != (a[j] % 2) {
+        }
+    }
